0db8b1a7d5184caa77971a48c761aed1ade56816
abbas <abbasvajwana1@gmail.com>
2024-08-28 17:41:42 +0530

    new

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://arty-booking-app.vercel.app', // Only allow requests from this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
+    allowedHeaders: ['Content-Type'], // Allow specific headers
+  }));
 
 // Connect to MongoDB
 mongoose.connect("mongodb+srv://abbasvajwana1:abbasatlas77@cluster1.0bhubyy.mongodb.net/travelDB", { useNewUrlParser: true, useUnifiedTopology: true })
